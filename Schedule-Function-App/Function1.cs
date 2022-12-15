e49d6302069fd2851a6623d85ea6a259feca2f94
Luke Sweeney <lukesweeney00@gmail.com>
2022-12-15 10:25:02 +0000

    Add SQL database and code to interact with it

--- Schedule-Function-App/Function1.cs
+++ Schedule-Function-App/Function1.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
 namespace Schedule_Function_App
 {
@@ -17,6 +19,21 @@ namespace Schedule_Function_App
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var query = "UPDATE SalesLT.SalesOrderHeader " +
+                        "SET [Status] = 4  WHERE ShipDate < GetDate();";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var rows = await cmd.ExecuteNonQueryAsync();
+                    log.LogInformation($"{rows} rows were updated");
+                }
+            }
+
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
