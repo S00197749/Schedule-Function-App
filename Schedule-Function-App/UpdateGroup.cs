f82bcdf57eb0496eb9cf605a59496c9713ca2b15
Luke Sweeney <lukesweeney00@gmail.com>
2023-03-29 20:19:11 +0100

    Added UpdateGroup

--- Schedule-Function-App/UpdateGroup.cs
+++ Schedule-Function-App/UpdateGroup.cs
@@ -11,25 +11,32 @@ using System.Data.SqlClient;
 
 namespace Schedule_Function_App
 {
-    public static class Update
+    public static class UpdateGroup
     {
-        [FunctionName("Update")]
+        [FunctionName("UpdateGroup")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string status = req.Query["status"];
+            int group_id = int.Parse(req.Query["group_id"]);
+            string group_name = req.Query["group_name"];
+            string group_desc = req.Query["group_desc"];
 
-            if(status != null) { 
+            if (group_id != null && group_name != null && group_desc != null) { 
                 var str = Environment.GetEnvironmentVariable("sqldb_connection");
                 using (SqlConnection conn = new SqlConnection(str))
                 {
                     conn.Open();
-                    var query = "UPDATE SalesLT.SalesOrderHeader " +
-                            $"SET [Status] = {status}  WHERE ShipDate < GetDate();";
+                    var query = "UPDATE Groups " +
+                            "SET Group_Name = @Group_Name, Group_Desc = @Group_Desc " +
+                                "WHERE Group_Id = @Group_Id;";
 
                     using (SqlCommand cmd = new SqlCommand(query, conn))
                     {
+                        cmd.Parameters.AddWithValue("@Group_Id", group_id);
+                        cmd.Parameters.AddWithValue("@Group_Name", group_name);
+                        cmd.Parameters.AddWithValue("@Group_Desc", group_desc);
+
                         // Execute the command and log the # rows affected.
                         var rows = await cmd.ExecuteNonQueryAsync();
                         log.LogInformation($"{rows} rows were updated");
@@ -42,7 +49,7 @@ namespace Schedule_Function_App
             }
             else
             {
-                string responseMessage = "This HTTP triggered function executed successfully. Pass a Username and Password in the query string or in the request body for a response.";
+                string responseMessage = "This HTTP triggered function executed successfully. Pass Group info in the query string or in the request body for a response.";
 
                 return new OkObjectResult(responseMessage);
             }
